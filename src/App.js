d1004196e6d5128948a8a04f0ccb79becb1c2280
Fridry <fredwin.ed@gmail.com>
2020-03-19 03:02:35 -0300

    Versão final

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import User from "./components/users/User";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false
-  };
-
-  async componentDidMount() {
-    this.setState({ loading: true });
-
-    const res = await axios.get("https://api.github.com/users");
-
-    this.setState({ users: res.data, loading: true });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+                />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
